d0bd2c59d982d5c54bca42f6bb03d7b4a09fe6ed
Shashank <shashankpateriya4@gmail.com>
2021-07-06 21:54:01 +0530

    [Shashank] Add:Snake and ladder with player at starting position

---
+++ SnakeAndLadder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SnakeLadder
+{
+    public class SnakeAndLadder
+    {
+        public static void Main(string[] args)
+        {
+            int positionOfPlayer = 0;
+            Console.WriteLine("Welcome to the game of SNAKE AND LADDER");
+            Console.WriteLine("Starting position of the player is at " + positionOfPlayer);
+        }
+    }
+}
