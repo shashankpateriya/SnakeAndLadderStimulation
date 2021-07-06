66033b48536813e8ffa99f088d75a3bd023e4866
Shashank <shashankpateriya4@gmail.com>
2021-07-06 22:27:07 +0530

    [Shashank] Add: Roll a dice to get number between 1 to 6

--- SnakeAndLadder.cs
+++ SnakeAndLadder.cs
@@ -7,8 +7,12 @@ namespace SnakeLadder
         public static void Main(string[] args)
         {
             int positionOfPlayer = 0;
+            int rollDice = 0;
             Console.WriteLine("Welcome to the game of SNAKE AND LADDER");
             Console.WriteLine("Starting position of the player is at " + positionOfPlayer);
+            Random random = new Random();
+            rollDice = random.Next(1, 7);
+            Console.WriteLine("Dice Number is " + rollDice);
         }
     }
 }
